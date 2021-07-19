5217d35ba5077a9cddc4460a3ce22db9b33f8be1
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2021-07-19 22:17:01 +0800

    Add files via upload

--- R/download_geo_series_matrix.R
+++ R/download_geo_series_matrix.R
@@ -3,17 +3,20 @@
 #' @param ... one or more gse
 #' @param chrome logical, whether to download by chrome
 #' @param sleep wait second, default is 5
+#' @param browser browser
 #' @return series matrix url
 #' @export
 #'
-download_geo_series_matrix <- function(...,chrome=FALSE,sleep=5){
+download_geo_series_matrix <- function(...,chrome=FALSE,sleep=5,browser = getOption("browser")){
     gse <- c(...)
     urls <- geo_ftp.URL(gse) |>
         unlist() |>
         sprintf(fmt = '%smatrix')
 
     for (i in do::seq_range(urls)) {
+        # print(i)
         if (i==1) mtrurl <- c()
+        urli <- urls[i]
         html <- tryCatch(xml2::read_html(urli),error=function(e) 'e')
         if (all(html=='e')) next(i)
         mtr <- html |>
@@ -23,7 +26,7 @@ download_geo_series_matrix <- function(...,chrome=FALSE,sleep=5){
         mtri <- paste0(urli,'/',mtr)
         if (chrome){
             for (i in mtri) {
-                browseURL(i)
+                browseURL(i,browser =browser )
                 Sys.sleep(sleep)
             }
         }
